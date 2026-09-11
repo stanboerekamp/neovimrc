fdfbf16f2568e0a8be05e5bd902d98df8882de9b
Stan Boerekamp <32361637+stanboerekamp@users.noreply.github.com>
2026-09-11 10:28:07 +0200

    awesome remaps

--- lua/stan/remap.lua
+++ lua/stan/remap.lua
@@ -1,7 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ss", vim.cmd.Ex)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
